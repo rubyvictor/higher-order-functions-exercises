f1ccec1ee61e63490ed976b964a03a06c320238c
Victor Lee <myleevictor@gmail.com>
2018-03-07 09:27:36 +0800

    updated solutions in map

--- map/core.js
+++ map/core.js
@@ -3,17 +3,16 @@ function multiplyBy10(array) {
 }
 
 function onlyVowels(array) {
-  return array.filter(
-    element => element.includes("a", "e", "i", "o", "u") === true
-  );
+  return array.map(element => element.match(/[aeiou]/gi) || []);
+  return array.map(element => element.split("a").join('').split("e").join('').split("i").join("").split("o").join("").split("u").join(""));
 }
 
 function doubleMatrix(array) {
-  return array;
+  return array.map((element,index,array) => element * 2);
 }
 
 function onlyNames(array) {
-  return array;
+  return array.map(x  => x["name"]);
 }
 
 module.exports = {
